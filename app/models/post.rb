6a59726e9d08b8480c7eb984e755b387e9ad294d
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:34:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,6 @@
-class Avalidator < ActiveModel::Validator
-  def validate(title)
-    arr = ["Won't Believe", "Secret", "Top", "Guess"]
-    title
-    unless %w().include?(arr) == true
-      record.errors[:name] << 'Need a name starting with X please!'
-    end
-  end
-end
 
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
   validates :title, presence: true
-  validates_with Avalidator
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}

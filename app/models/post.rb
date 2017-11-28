22324057fa55454ec978f31ab767e1a43f112b21
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:41:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,10 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :is_clickbait?
 
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
+  
 
   def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    if /Won't Believe/.match(title)
       errors.add(:title, "must be clickbait")
     end
   end

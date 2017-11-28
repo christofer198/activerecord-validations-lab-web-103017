5ff8a6248d07054e542dba4c347c876a2809f915
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:24:50 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ end
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates :title, presence: true
-  validates_with Avalidator
+  validates_with Avalidator :title
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}

0fb55b70cb0e951826199ee01b037852524c166b
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:45:14 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
 
   def is_clickbait?
     binding.pry
-    if /Won't/.match(title) != true
+    if /Won't/.match(title) == nil
       errors.add(:title, "There was not a match")
     end
   end
